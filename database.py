e9f697d493d92da5f250ddc4b49b5c3ae1b171ab
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-07-05 16:25:51 +0530

    declarative base setup

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread":False})
+
+Base = declarative_base()
