2a86d99a84f04c492c54c38d8d2e477322118d04
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-07-05 14:59:14 +0530

    query run on local host

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':"Divyanshi"}}
+@app.get('/blog')
+def index(limit):
+    return {'data':f'{limit} blogs from the database'}
 
 @app.get('/about')
 def about():
-    return {'data':'about page'}
+    return {'data':'about blog page'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'unpublished blog data here'}
+
 
 @app.get('/blog/{id}')
 def show(id:int):
     return {'data': id}
 
-@app.get('/blog/unpublished')
-def unpublished():
-    return {'data':'unpublished data here'}
-
 
 @app.get('/blog/{id}/comments')
 def comments(id:int):
-    return {'data':{id:'comments'}}
+    return {'data':{id:'comments on blog'}}
