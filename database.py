e988c90e6a764bbd193437f68780ebe2c99ba0bd
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-07-06 00:28:09 +0530

    database connection done

--- database.py
+++ database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
 
+# represent the core interface to the database
 engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread":False})
 
+SessionLocal =  sessionmaker(bind=engine , autocommit=False , autoflush=False)
+
+# allow us to create classes that include directives to describe the actual database table they will be mapped to.
 Base = declarative_base()
+
+
+
