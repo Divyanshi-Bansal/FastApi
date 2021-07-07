2669791b2acf7718c0113d26a76ed4b889c24a01
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-07-08 00:35:34 +0530

    pass creator info in showBlog

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def show(id:int ,response:Response , db:Session = Depends(get_db)):
 def createBlog(request: schemas.Blog , db:Session = Depends(get_db)):
     # return {'data':f"Blog is ceated {request}"}
     # newBlog is going to schemas
-    newBlog = models.Blog(id = request.id , title = request.title , body = request.body , name = request.name, published=request.published)
+    newBlog = models.Blog(title = request.title , body = request.body , name = request.name, published=request.published , userId=1)
     db.add(newBlog)
     db.commit()
     db.refresh(newBlog)
--- schemas.py
+++ schemas.py
@@ -3,20 +3,11 @@ from typing import Optional
 
 class Blog(BaseModel):
 
-    id:int
     name: str
     title: str
     body: str
     published: Optional[bool]
 
-class ShowBlog(BaseModel):
-
-    name:str
-    title:str
-    published:Optional[bool]
-
-    class Config():
-        orm_mode= True
 
 
 class User(BaseModel):
@@ -34,3 +25,13 @@ class ShowUser(BaseModel):
 
     class Config():
         orm_mode =True
+
+class ShowBlog(BaseModel):
+
+    name:str
+    title:str
+    creator:ShowUser
+    published:Optional[bool]
+
+    class Config():
+        orm_mode= True
