e080b76799f97a8a906fa9df1b4423eb7c9a27a1
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-07-08 15:16:03 +0530

    token & tokendata class in schemas.py

--- schemas.py
+++ schemas.py
@@ -47,3 +47,12 @@ class login(BaseModel):
     name:str
     email:str
     password:str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
